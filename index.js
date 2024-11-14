a8ae7c4e5b297018b28e33e1c0a5f83c44c0a478
maryroseeee <91707406+maryroseeee@users.noreply.github.com>
2024-11-14 00:04:33 -0500

    k

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const app = express()
-app.get('/hello', (req, res) => {res.send('Life is good!')})
-app.get('/', (req, res) => {
-    res.send('Welcome to Full Stack Development!')})
-app.listen(4000)
+import Lab5 from "./Lab5/index.js";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+Lab5(app);
+app.listen(4000);
