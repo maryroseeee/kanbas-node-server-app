885907aba64028189b8dd867756fbe6009085f10
maryroseeee <91707406+maryroseeee@users.noreply.github.com>
2024-11-16 14:35:20 -0500

    up until 4.6

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
+import express from "express";
 import Lab5 from "./Lab5/index.js";
+import UserRoutes from "./Kanbas/Users/routes.js";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
 import cors from "cors";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
+
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+             credentials: true,
+             origin: process.env.NETLIFY_URL || "http://localhost:3000",
+         })
+);
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kanbas",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.NODE_SERVER_DOMAIN,
+    };
+}
 app.use(express.json());
 Lab5(app);
+UserRoutes(app);
+CourseRoutes(app);
+EnrollmentRoutes(app);
+ModuleRoutes(app);
 app.listen(4000);
