784df085e7894162df52f731f762b7e2518be408
maryroseeee <91707406+maryroseeee@users.noreply.github.com>
2024-11-22 20:38:51 -0500

    up until 4.6

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const sessionOptions = {
     resave: false,
     saveUninitialized: false,
 };
+
 if (process.env.NODE_ENV !== "development") {
     sessionOptions.proxy = true;
     sessionOptions.cookie = {
@@ -30,10 +31,7 @@ if (process.env.NODE_ENV !== "development") {
         domain: process.env.NODE_SERVER_DOMAIN,
     };
 }
-
-app.use(
-    session(sessionOptions)
-);
+app.use(session(sessionOptions));
 
 app.use(express.json());
 Lab5(app);
